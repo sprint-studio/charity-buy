9cdc21251397fa0d246960f60ca033bc79942947
thekage91 <the--kage91@hotmail.it>
2020-03-25 00:13:16 +0100

    Added alert message if the user insert not valid link

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@
     $(document).ready(function() {
         $('form[name="link-generator"]').submit(function() {
             let amazonLink = $('input[name="amazon-link"]').val().split('?')[0];
+
+            if(amazonLink.indexOf('amazon') === -1) {
+                return alert("Attenzione, sembra che tu abbia inserito un link non valido.   Ricorda che devi inserire il link di un prodotto su Amazon.");
+            }
+            
             window.open(amazonLink + '?tag=873401-21', 'blank')
         });
     });
