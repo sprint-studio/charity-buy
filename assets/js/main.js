37ee9999cb424adbc5cfa42976ed066c524e3d80
thekage91 <the--kage91@hotmail.it>
2020-03-30 16:59:26 +0200

    Updated login flow with Facebook and added anchor to call to action box

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,28 +6,36 @@
 
     if (client.auth.hasRedirectResult()) {
       client.auth.handleRedirectResult().then(function(user) {
-        $('[name="user"] a').val(user.profile.name);
+        $('[name="user"]').removeAttr("hidden");
+        $('[name="user"] a').html(user.profile.name);
       });
     }
 
+    
     if (client.auth.isLoggedIn) {
       let user = client.auth.user;
-      console.log(client.auth.user.profile);
+      $('[name="user"]').removeAttr("hidden");
+      $('.btn-login-facebook').attr("hidden", "");
+      $('.btn#join').attr("hidden", "");
 
-      $('[name="user"] a').html(user.profile.name);
-    } else {
-      client.auth
+      console.log(user.profile.name);
+      $('[name="user"] a').first().html(user.profile.name);
+    } 
+
+    $('form[name="link-generator"] .btn').click(function() {
+      let amazonLink = $('input[name="amazon-link"]').val();
+
+      if(!client.auth.isLoggedIn) {
+        client.auth
         .loginWithCredential(new stitch.AnonymousCredential())
         .then(user => {
-          console.log(user);
+          //console.log(user);
         })
         .catch(err => {
           console.error(err);
         });
-    }
+      }
 
-    $('form[name="link-generator"] .btn').click(function() {
-      let amazonLink = $('input[name="amazon-link"]').val();
       client
         .callFunction("Generate_tracking_link", [amazonLink])
         .then(function(result) {
@@ -39,10 +47,18 @@
         });
     });
 
-    $(".btn#join").click(function() {
+    $(".btn.btn-login-facebook").click(function() {
       const credential = new stitch.FacebookRedirectCredential();
+
       client.auth.logout();
       client.auth.loginWithRedirect(credential);
+    });
+
+    $(".btn-logout").click(function() {
+      client.auth.logout()
+      .then(function() {
+        location.reload();
+      });
     });
   });
 })();
